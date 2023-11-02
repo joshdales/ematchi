0bda75018707193cd741db41dc11f701d13224e0
Josh Dales <git@joshdales.dev>
2023-11-02 17:11:24 -0400

    Start adding game logic

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
 	import Square from './Square.svelte';
 	export let grid: string[];
+
+	let a: number = -1;
+	let b: number = -1;
 </script>
 
 <div class="grid">
-	{#each grid as emoji}
-		<Square {emoji} />
+	{#each grid as emoji, i}
+		<Square
+			{emoji}
+			selected={a === i || b === i}
+			on:click={() => {
+				if (a === -1 && b === -1) {
+					a = i;
+				}
+			}}
+		/>
 	{/each}
 </div>
 
--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
+	export let selected: boolean;
 	export let emoji: string;
 </script>
 
-<div class="square">{emoji}</div>
+<div class="square" class:flipped={selected}>
+	<button on:click />
+	<span>{emoji}</span>
+</div>
 
 <style>
 	div {
-		background-color: yellow;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.flipped button {
+		background-color: red;
+	}
+
+	button {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	span {
+		font-size: 5em;
 	}
 </style>
